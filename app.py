d75a5c2101bea0dbe4bbb27b4c0518954e0f7e20
AdluAghnia <adluaghnia03@gmail.com>
2024-11-03 07:09:00 +0700

    Batman

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    data = {
+        "message" : "Hello World",
+    }
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
